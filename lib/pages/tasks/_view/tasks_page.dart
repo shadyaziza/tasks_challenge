ddb80605abb78f68219cf4c2182962b9293253b8
Shady Aziza <shadyaziza@gmail.com>
2024-09-26 13:36:24 +0300

    add starter project

---
+++ lib/pages/tasks/_view/tasks_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TasksPage extends StatelessWidget {
+  const TasksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // TODO: labels should be in app localization file
+      child: Text('Tasks'),
+    );
+  }
+}
---
+++ lib/pages/teams/_view/teams_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TeamsPage extends StatelessWidget {
+  const TeamsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // TODO: labels should be in app localization file
+      child: Text('Teams'),
+    );
+  }
+}
